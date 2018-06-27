c7967f169b4efd2e000256ec650b038f5e3744b4
matl2018 <lumierematerne11@gmail.com>
2018-06-27 20:49:38 +0100

    "add contact handlebars and partials "

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const app = express();
+const exphbs = require("express-handlebars");
 
-// The extensions 'html' allows us to serve file without adding .html at the end 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    adminLink: "main admin"
+  });
+});
+app.get("/my-cv", (req, res) => {
+  res.render("my-cv");
+});
+app.get("/admin", (req, res) => {
+  res.render("admin");
+});
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+// The extensions 'html' allows us to serve file without adding .html at the end
 // i.e /my-cv will server /my-cv.html
-app.use(express.static("public", {'extensions': ['html']}));
 
+app.use(express.static("public", { extensions: ["html"] }));
+app.get("/contact", function(req, res) {
+  res.send("message for contact us");
+});
 
 // what does this line mean: process.env.PORT || 3000
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
